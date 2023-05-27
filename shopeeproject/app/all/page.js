c44e9913403585acd0314f613935fe068c997b29
Pitchaya Oat <znxxx@Pitchayas-MacBook-Air-2.local>
2023-05-28 02:53:54 +0700

    first-commit

---
+++ shopeeproject/app/all/page.js
@@ -0,0 +1,89 @@
+"use client";
+import { useState, useEffect } from "react";
+// import data from "../data.json";
+import axios from "axios";
+
+export default function All() {
+  const [filterOne, setFilterOne] = useState("");
+  const [filterTwo, setFilterTwo] = useState("");
+  const [filterThree, setFilterThree] = useState("");
+  const [filterFour, setFilterFour] = useState("");
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState("1");
+
+  useEffect(() => {
+    getData()
+  }, []);
+  function getData() {
+    fetch(`http://localhost:3000/backendTest/account/pages/${page}`, {
+      method: "get",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) =>
+        // console.log("res", res);
+        res.json()
+      )
+      .then((response) => setData(response.result))
+      .catch((err) => {
+        console.error("err", err);
+      });
+  }
+
+  return (
+    <div>
+      <div className="grid grid-cols-4 gap-4 justify-items-center">
+        <div className="bg-yellow-100 border-2 border-black p-2 rounded-md">
+          <input
+            type="checkbox"
+            onChange={() =>
+              filterOne ? setFilterOne("") : setFilterOne("safe")
+            }
+          />
+          MR.A <br />
+          <input
+            type="checkbox"
+            onChange={() =>
+              filterTwo ? setFilterTwo("") : setFilterTwo("oat")
+            }
+          />
+          MR.B <br />
+          <input
+            type="checkbox"
+            onChange={() =>
+              filterThree ? setFilterThree("") : setFilterThree("MR.C")
+            }
+          />
+          MR.C <br />
+          <input
+            type="checkbox"
+            onChange={() =>
+              filterFour ? setFilterFour("") : setFilterFour("MR.D")
+            }
+          />
+          MR.D <br />
+        </div>
+        <div className="col-span-3 grid grid-cols-2 gap-16 border-2 border-black p-2 w-full m-2 bg-green-50">
+          <button onClick={() => getData()}>click</button>
+          {data &&
+            data
+              .filter((d) => {
+                return (
+                  d.user === filterOne ||
+                  d.user === filterTwo ||
+                  d.user === filterThree ||
+                  d.user === filterFour
+                );
+              })
+              .map((d) => (
+                <div className="bg-green-50">
+                  <p>{d.user}</p>
+                  <p>{d.product_name}</p>
+                  {/* <p>{d.Picture}</p> */}
+                  <p>{d.photo_description}</p>
+                </div>
+              ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ shopeeproject/app/page.js
@@ -0,0 +1,89 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  const [user, setUser] = useState();
+  const [product, setProduct] = useState();
+  const [picture, setPicture] = useState();
+  const [description, setDescription] = useState();
+  const [base64IMG, setBase64IMG] = useState();
+
+  const convertToBase64 = (selectedFile) => {
+    console.log(selectedFile)
+    const reader = new FileReader()
+
+    reader.readAsDataURL(selectedFile)
+
+    reader.onload = () => {
+      console.log('called: ', reader)
+      setBase64IMG(reader.result)
+    }
+  }
+
+  function handleSubmit() {
+    let data = {
+      user: user,
+      prod_name: product,
+      picture: base64IMG,
+      photo: description,
+    };
+    fetch("http://localhost:3000/backendTest/users/postUser", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((err) => {
+        console.error("err", err);
+      });
+  }
+  let styleTitle = { color: "red" };
+  return (
+    <div className="w-full bg-[green]">
+      <div className="red">
+        User{" "}
+        <input
+          className="border-2 border-black "
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+      </div>
+      <div style={{ color: "red" }}>
+        Product{" "}
+        <input
+          className="border-2 border-black"
+          type="text"
+          value={product}
+          onChange={(e) => setProduct(e.target.value)}
+        />
+      </div>
+      <div style={styleTitle}>
+        Picture{" "}
+        <input
+          type="file"
+          onChange={(e) => {setPicture(URL.createObjectURL(e.target.files[0])), convertToBase64(e.target.files[0])}}
+        />
+      </div>
+      <div>
+        Description{" "}
+        <input
+          className="border-2 border-black"
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+      <img src={picture}/>
+      <button onClick={() => handleSubmit()}>submit</button>
+      <img
+        src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
+    AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
+        9TXL0Y4OHwAAAABJRU5ErkJggg=="
+        alt="Red dot"
+      />
+    </div>
+  );
+}
